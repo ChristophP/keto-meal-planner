c1b664d608d88a9c696e66b2ca28f9c36bf4b157
ChristophP <deedop@hotmail.de>
2020-03-17 22:38:52 +0100

    refactor view helpers

--- src/View/Helpers.elm
+++ src/View/Helpers.elm
@@ -6,18 +6,6 @@ import Html.Events exposing (onClick)
 import View.Icons as Icons
 
 
-attrList : List ( Attribute msg, Bool ) -> List (Attribute msg)
-attrList =
-    List.filterMap
-        (\( attr, include ) ->
-            if include then
-                Just attr
-
-            else
-                Nothing
-        )
-
-
 attrIf : Bool -> Attribute msg -> Attribute msg
 attrIf show attr =
     if show then
@@ -27,6 +15,7 @@ attrIf show attr =
         classList []
 
 
+inputField : List (Attribute msg) -> List (Html msg) -> Html msg
 inputField attr =
     input
         (class "w-full p-2 shadow focus:shadow-outline" :: attr)
@@ -41,27 +30,21 @@ dialog :
     -> Html msg
 dialog { show, title, content, onClose } =
     div
-        (attrList
-            [ ( class "fixed inset-0 flex items-center justify-center w-screen h-screen p-4", True )
-            , ( style "transform" "translateY(-100vh)", not show )
-            ]
-        )
+        [ class "fixed inset-0 flex items-center justify-center w-screen h-screen p-4"
+        , attrIf (not show) <| style "transform" "translateY(-100vh)"
+        ]
         [ div
-            (attrList
-                [ ( class "absolute inset-0 bg-black opacity-50", True )
-                , ( class "transition-opacity duration-500", True )
-                , ( onClick onClose, True )
-                , ( style "opacity" "0", not show )
-                ]
-            )
+            [ class "absolute inset-0 bg-black opacity-50"
+            , class "transition-opacity duration-500"
+            , onClick onClose
+            , attrIf (not show) <| style "opacity" "0"
+            ]
             []
         , div
-            (attrList
-                [ ( class "z-10 flex flex-col w-full h-full bg-white rounded-lg shadow-md", True )
-                , ( class "transition-transform duration-500", True )
-                , ( style "transform" "translateY(-100vh)", not show )
-                ]
-            )
+            [ class "z-10 flex flex-col w-full h-full bg-white rounded-lg shadow-md"
+            , class "transition-transform duration-500"
+            , attrIf (not show) <| style "transform" "translateY(-100vh)"
+            ]
             [ div [ class "relative flex items-center justify-center py-2 text-xl border-b border-gray-400" ]
                 [ text title
                 , button [ class "absolute right-0 w-4 mr-2", onClick onClose ]
