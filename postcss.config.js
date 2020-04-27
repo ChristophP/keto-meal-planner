5b715b4a42b845bd5f4970eaeb6b50e08291f55a
ChristophP <deedop@hotmail.de>
2020-04-27 21:22:58 +0200

    update tailwind, add purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,17 @@
 /* eslint-disable import/no-extraneous-dependencies, global-require */
 
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  // Specify the paths to all of the template files in your project
+  content: ["./src/**/*.elm"],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+});
+
 module.exports = {
   plugins: [
-    // require("postcss-import"),
     require("tailwindcss"),
-    require("autoprefixer")
+    require("autoprefixer"),
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
   ]
 };
